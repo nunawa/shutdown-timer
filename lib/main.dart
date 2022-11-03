9cd406cfc7a0f15b46feed48a4eeb61dcc17ec01
nunawa <71294849+nunawa@users.noreply.github.com>
2022-11-03 21:00:37 +0900

    Use fluent_ui and bitsdojo_window

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+
+const title = "シャットダウンタイマー";
 
 void main() {
   runApp(const MyApp());
+
+  doWhenWindowReady(() {
+    const initialSize = Size(800, 600);
+    appWindow.minSize = initialSize;
+    appWindow.size = initialSize;
+    appWindow.alignment = Alignment.center;
+    appWindow.title = title;
+    appWindow.show();
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -10,21 +22,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const FluentApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      home: MyHomePage(title: "$title Home Page"),
     );
   }
 }
@@ -49,6 +50,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int topIndex = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -69,47 +71,49 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return NavigationView(
+      appBar: NavigationAppBar(
+          title: const Text(title),
+          height: 40,
+          actions: MoveWindow(
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [Spacer(), WindowButtons()]),
+          )),
+      pane: NavigationPane(
+          selected: topIndex,
+          onChanged: (index) => setState(() => topIndex = index),
+          displayMode: PaneDisplayMode.auto,
+          items: [
+            PaneItem(
+              icon: const Icon(FluentIcons.home),
+              title: const Text("ホーム"),
+              body: Container(
+                color: Colors.black,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            PaneItem(
+              icon: const Icon(FluentIcons.mail),
+              title: const Text("メール"),
+              body: Container(
+                color: Colors.black,
+              ),
+            )
+          ]),
+    );
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(height: double.infinity, child: MinimizeWindowButton()),
+        SizedBox(height: double.infinity, child: MaximizeWindowButton()),
+        SizedBox(height: double.infinity, child: CloseWindowButton()),
+      ],
     );
   }
 }
